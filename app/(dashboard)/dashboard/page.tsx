b45f886e0385bdc5e885473477bb295659aea0a8
Heinrick Senna <marcelohz198@hotmail.com>
2024-07-05 20:24:33 -0300

    Update

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -12,105 +12,51 @@ import {
 } from '@/components/ui/card';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import Link from 'next/link';
 
 export default function page() {
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
         <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Bem-vindo 👋</h2>
-          <div className="hidden items-center space-x-2 md:flex">a</div>
+          <h2 className="text-3xl font-bold tracking-tight">Meus Clientes</h2>
         </div>
         <Tabs defaultValue="overview" className="space-y-4">
           <TabsList>
             <TabsTrigger value="overview">Overview</TabsTrigger>
+            <TabsTrigger value="hubspot">Hubspot</TabsTrigger>
             <TabsTrigger value="analytics" disabled>
               Analytics
             </TabsTrigger>
           </TabsList>
+
           <TabsContent value="overview" className="space-y-4">
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-              <CardWhatsapp />
               <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Subscriptions
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+2350</div>
-                  <p className="text-xs text-muted-foreground">
-                    +180.1% from last month
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">Sales</CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <rect width="20" height="14" x="2" y="5" rx="2" />
-                    <path d="M2 10h20" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+12,234</div>
-                  <p className="text-xs text-muted-foreground">
-                    +19% from last month
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Active Now
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+573</div>
-                  <p className="text-xs text-muted-foreground">
-                    +201 since last hour
-                  </p>
-                </CardContent>
+                <CardWhatsapp />
               </Card>
             </div>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7">
-              lista de descrição
-            </div>
+          </TabsContent>
+          <TabsContent value="hubspot" className="space-y-4">
+            <Button>
+              <Link
+                href={
+                  'https://app.hubspot.com/oauth/authorize' +
+                  `?client_id=${encodeURIComponent(
+                    `a726326a-9d26-435a-a401-dc847b13710f`
+                  )}` +
+                  `&scope=${encodeURIComponent(`contacts%20social`)}` +
+                  `&redirect_uri=${encodeURIComponent(
+                    `http://localhost:3000/hello/Marcelo`
+                  )}` +
+                  `&state=${encodeURIComponent(
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbHk5OWJub3owMDAwM3BzZWNkenB3NmNxIiwiaWF0IjoxNzIwMjE3OTAxLCJleHAiOjE3MjE1MTM5MDF9.V2l4YCFgGL0yFynscqaSt-fR8tS4BFuR-4Kc8VaZoaU'
+                  )}`
+                }
+              >
+                Configurar
+              </Link>
+            </Button>
           </TabsContent>
         </Tabs>
       </div>
--- components/dashboard-components/cardWhatsapp.tsx
+++ components/dashboard-components/cardWhatsapp.tsx
@@ -33,23 +33,11 @@ export default function CardWhatsapp() {
   console.log(data);
 
   return (
-    <Card>
+    <>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm font-medium">
           Status do WhatsApp
         </CardTitle>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="h-4 w-4 text-muted-foreground"
-        >
-          <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-        </svg>
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">
@@ -61,11 +49,7 @@ export default function CardWhatsapp() {
             'Buscando QRcode...'
           )}
         </div>
-
-        {/* <p className="text-xs text-muted-foreground">
-            Reconectar ou configurar novamente.
-        </p> */}
       </CardContent>
-    </Card>
+    </>
   );
 }
